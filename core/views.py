e6059e2ee1cd686bf0dd623d83c9be573bda23bc
akashdeep-19 <akasdeeps19@gmail.com>
2020-04-13 20:08:34 +0530

    Added follower count check

--- core/views.py
+++ core/views.py
@@ -32,20 +32,27 @@ def insta(request):
 REQUEST_ACCESS = "https://api.instagram.com/oauth/access_token/?"
 REDIRECT_URI = "https://localhost:8000/auth/insta"
 def grant_access(request):
-	print("something")
-	code = request.GET.get('code')
-	payload = {'client_id': '2651264628452014', 'client_secret':'69caad94f800eac62357bc5783b164c6', 'grant_type':'authorization_code','redirect_uri': REDIRECT_URI, 'code': code}
-	resp = requests.post(REQUEST_ACCESS, data= payload)
-	response = json.loads(resp.text) 
-	# print(response['user_id'],resp['access_token'],"Thisssss")
-	# print(response)
-	url=('https://graph.instagram.com/{}?fields=id,username&access_token={}').format(response['user_id'],response['access_token'])
-	# print( HttpResponseRedirect(url))
-	resp=requests.get(url)
-	user_dictionary = json.loads(resp.text) 
-	print(user_dictionary)
+    print("something")
+    code = request.GET.get('code')
+    payload = {'client_id': '2651264628452014', 'client_secret':'69caad94f800eac62357bc5783b164c6', 'grant_type':'authorization_code','redirect_uri': REDIRECT_URI, 'code': code}
+    resp = requests.post(REQUEST_ACCESS, data= payload)
+    response = json.loads(resp.text) 
+    # print(response['user_id'],resp['access_token'],"Thisssss")
+    # print(response)
+    url=('https://graph.instagram.com/{}?fields=id,username&access_token={}').format(response['user_id'],response['access_token'])
+    # print( HttpResponseRedirect(url))
+    resp=requests.get(url)
+    user_dictionary = json.loads(resp.text) 
+    print(user_dictionary)
+    # url2 = f"https://graph.instagram.com/{user_dictionary['id']}?fields=followers_count&access_token={response['access_token']}"
+    url2 = f"https://www.instagram.com/{user_dictionary['username']}/?__a=1"
+    resp2=requests.get(url2)
+    res = json.loads(resp2.text)
+    print(res)
+    user_dictionary['followers_count'] = res['graphql']['user']['edge_followed_by']['count']
+
 	# return HttpResponseRedirect("https://localhost:8000/")
-	return render(request,'home.html',context=user_dictionary)
+    return render(request,'home.html',context=user_dictionary)
 
     
 	# return HttpResponseRedirect(url)
