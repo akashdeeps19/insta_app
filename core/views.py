c27f4f1e7ff379b23c1fd2e892b943ceeeac4150
akashdeep-19 <akasdeeps19@gmail.com>
2020-04-14 13:01:58 +0530

    cleaned up code

--- core/views.py
+++ core/views.py
@@ -3,37 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 import requests,json
 from django.urls import reverse
-
-user_dictionary={}
-# Create your views here.
-def login(request):
-  return render(request, 'login.html')
-
-@ login_required
-def home(request):
-	print(user_dictionary,"insideee")
-	return render(request, 'home.html', context=user_dictionary)
-
-
-REQUEST_ACCESS = "https://api.instagram.com/oauth/access_token/?"
-# def grant_access(request):
-#     code = request.GET.get('code')
-#     payload = {'client_id': CLIENT_ID, 'client_secret':CLIENT_SECRET, 'grant_type':'authorization_code','redirect_uri': REDIRECT_URI, 'code': code}
-#     resp = requests.post(REQUEST_ACCESS, data= payload)
-#     response = json.loads(resp.text)
-
-def insta(request):
-	# https://www.instagram.com/oauth/authorize?client_id=2651264628452014&redirect_uri=https://localhost:8000/auth/insta&scope=user_profile,user_media&response_type=code
-	BASE_URL = "https://www.instagram.com/oauth/authorize/?"
-	REDIRECT_URI = "https://localhost:8000/auth/insta"
-	url = BASE_URL + "client_id={}&redirect_uri={}&scope=user_profile,user_media&response_type=code".format('2651264628452014',REDIRECT_URI)
-	print(url)
-	return HttpResponseRedirect(url)
-
-REQUEST_ACCESS = "https://api.instagram.com/oauth/access_token/?"
-REDIRECT_URI = "https://localhost:8000/auth/insta"
-
 import os
+
 def env_var(key, default=None):
     """Retrieves env vars and makes Python boolean replacements"""
     val = os.environ.get(key, default)
@@ -43,36 +14,47 @@ def env_var(key, default=None):
         val = False
     return val
 
+AUTH_URL = "https://www.instagram.com/oauth/authorize/?"
+REQUEST_ACCESS_URL = "https://api.instagram.com/oauth/access_token/?"
+REDIRECT_URI = "https://localhost:8000/auth/insta"
+INSTAGRAM_KEY = env_var('INSTAGRAM_KEY')
+INSTAGRAM_SECRET = env_var('INSTAGRAM_SECRET')
+
+# Create your views here.
+def login(request):
+  return render(request, 'login.html')
+
+@ login_required
+def home(request):
+	return render(request, 'home.html')
+
+def insta(request):
+	url = f"{AUTH_URL}client_id={INSTAGRAM_KEY}&redirect_uri={REDIRECT_URI}&scope=user_profile,user_media&response_type=code"
+	return HttpResponseRedirect(url)
 
 def grant_access(request):
-    print("something")
+    # Get access token for the user
     code = request.GET.get('code')
-    payload = {'client_id': env_var('INSTAGRAM_KEY') , 'client_secret':env_var('INSTAGRAM_SECRET')  , 'grant_type':'authorization_code','redirect_uri': REDIRECT_URI, 'code': code}
-    resp = requests.post(REQUEST_ACCESS, data= payload)
-    response = json.loads(resp.text) 
-    # print(response['user_id'],resp['access_token'],"Thisssss")
-    print(response)
-    # 'code': 400
+    payload = {'client_id': INSTAGRAM_KEY , 'client_secret': INSTAGRAM_SECRET , 'grant_type':'authorization_code','redirect_uri': REDIRECT_URI, 'code': code}
+    res = requests.post(REQUEST_ACCESS_URL, data= payload)
+    response = json.loads(res.text) 
+    
+    # Auth failed then redirect to login
     if 'code' in response.keys():
     	if(response['code']==400):
-    		print("redirecting")
     		return HttpResponseRedirect(reverse('login'))
-    url=('https://graph.instagram.com/{}?fields=id,username&access_token={}').format(response['user_id'],response['access_token'])
-    # print( HttpResponseRedirect(url))
-    resp=requests.get(url)
-    user_dictionary = json.loads(resp.text) 
-    print(user_dictionary)
-    # url2 = f"https://graph.instagram.com/{user_dictionary['id']}?fields=followers_count&access_token={response['access_token']}"
-    url2 = f"https://www.instagram.com/{user_dictionary['username']}/?__a=1"
-    resp2=requests.get(url2)
-    res = json.loads(resp2.text)
-    print(res)
+
+    # Get user name
+    url = f"https://graph.instagram.com/{response['user_id']}?fields=id,username&access_token={response['access_token']}"
+    res =requests.get(url)
+    user_dictionary = json.loads(res.text) 
+
+    # Get followers count
+    url = f"https://www.instagram.com/{user_dictionary['username']}/?__a=1"
+    res =requests.get(url)
+    res = json.loads(res.text)
     user_dictionary['followers_count'] = res['graphql']['user']['edge_followed_by']['count']
 
-	# return HttpResponseRedirect("https://localhost:8000/")
     return render(request,'home.html',context=user_dictionary)
 
     
-	# return HttpResponseRedirect(url)
-
-	# return HttpResponse(response)
